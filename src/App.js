e889f88b54ee9261851ff66bd7134d2e5ffc343f
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-29 23:14:10 +0200

    added skeleton & loading functional

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://66071801be53febb857f2531.mockapi.io/items")
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,9 +33,11 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((obj, index) => {
-                return <PizzaBlock {...obj} key={index} />;
-              })}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                : items.map((obj) => {
+                    return <PizzaBlock key={obj.id} {...obj} />;
+                  })}
             </div>
           </div>
         </div>
